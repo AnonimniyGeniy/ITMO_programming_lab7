8f80afb57645ef9f8edc44353d0c2f6785083873
AnonimniyGeniy <68807643+AnonimniyGeniy@users.noreply.github.com>
2023-06-05 13:24:21 +0300

    started writing some db logic and planned user structure
    (cherry picked from commit 0dd731109a26b80fd5e3c522e88877d824680ebb)

--- server/src/server/App.java
+++ server/src/server/App.java
@@ -1,25 +1,25 @@
 package server;
 
 import managers.CollectionManager;
-import managers.DbManager;
-
+import managers.FileManager;
 
 public class App {
     public static void main(String[] args) {
+        String path = null;
         String user = null;
         try {
+            path = System.getenv("FILENAME");
             user = System.getenv("USER");
         } catch (Exception e) {
             System.out.println("Environment variable FILENAME is not set");
             System.exit(1);
         }
-
-        DbManager dbManager = new DbManager(user.equals("s367132"));
-        dbManager.connect();
-        dbManager.createTablesIfNotExist();
-        CollectionManager collectionManager = new CollectionManager(dbManager);
-
-
+        if (path == null) {
+            System.out.println("Environment variable FILENAME is not set");
+            System.exit(1);
+        }
+        FileManager fileManager = new FileManager(path);
+        CollectionManager collectionManager = new CollectionManager(fileManager);
         try {
             collectionManager.loadCollection();
         } catch (Exception e) {
@@ -27,7 +27,7 @@ public class App {
             System.exit(1);
         }
 
-        //password: 12345678, enc: ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f
+        System.out.println(user);
         Server server = new Server(collectionManager);
 
         server.run();
