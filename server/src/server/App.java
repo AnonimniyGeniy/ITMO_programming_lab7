1966826895742e77958e513ff7dc812e30fb23d8
AnonimniyGeniy <68807643+AnonimniyGeniy@users.noreply.github.com>
2023-06-14 18:49:59 +0300

    Revert "started writing some db logic and planned user structure"
    This reverts commit 8f80afb57645ef9f8edc44353d0c2f6785083873.

--- server/src/server/App.java
+++ server/src/server/App.java
@@ -1,25 +1,25 @@
 package server;
 
 import managers.CollectionManager;
-import managers.FileManager;
+import managers.DbManager;
+
 
 public class App {
     public static void main(String[] args) {
-        String path = null;
         String user = null;
         try {
-            path = System.getenv("FILENAME");
             user = System.getenv("USER");
         } catch (Exception e) {
             System.out.println("Environment variable FILENAME is not set");
             System.exit(1);
         }
-        if (path == null) {
-            System.out.println("Environment variable FILENAME is not set");
-            System.exit(1);
-        }
-        FileManager fileManager = new FileManager(path);
-        CollectionManager collectionManager = new CollectionManager(fileManager);
+
+        DbManager dbManager = new DbManager(user.equals("s367132"));
+        dbManager.connect();
+        dbManager.createTablesIfNotExist();
+        CollectionManager collectionManager = new CollectionManager(dbManager);
+
+
         try {
             collectionManager.loadCollection();
         } catch (Exception e) {
@@ -27,7 +27,7 @@ public class App {
             System.exit(1);
         }
 
-        System.out.println(user);
+        //password: 12345678, enc: ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f
         Server server = new Server(collectionManager);
 
         server.run();
